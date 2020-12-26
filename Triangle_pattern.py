d68214af6494ec08c902aff6c7889596d6e2a053
phaniramavarapu <rphani2018@gmail.com>
2020-12-26 16:08:32 +0530

    for loop

--- Triangle_pattern.py
+++ Triangle_pattern.py
@@ -1,3 +1,28 @@
+'''
 abc = [1,2,3,4,5,6]
 for i in abc:
     print(i)
+'''
+
+#Square pattern
+for number in range(1,6):
+    for number in range(1,6):
+        print(number, end="")
+    print()
+
+
+
+#Triangle Pattern
+
+n = int(input("Enter the number of rows: "))
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(i, end="")
+    print()
+
+
+
+
+
+
+
