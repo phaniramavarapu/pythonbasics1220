36fb647e8ee9fdb0eb581a885b46cf4484d7fd13
phaniramavarapu <rphani2018@gmail.com>
2020-12-27 13:19:42 +0530

    1227

---
+++ 1227.py
@@ -0,0 +1,21 @@
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# if a > b:
+#     print("a is greater than b")
+# elif a < b:
+#     print("a is less than b")
+# elif a == b:
+#     print("a is equal to b")
+#
+#
+
+language = 'Java'
+
+if language == 'Python':
+    print('Language is Python')
+elif language == 'Java':
+    print('Language is java')
+elif language == 'Javascript':
+    print('Language is javascript')
+else:
+    print('No match')
