6d1c01c32d543bea10ea51e919ad62c4a2c7603f
phaniramavarapu <rphani2018@gmail.com>
2020-12-28 12:56:04 +0530

    1228

--- 1227.py
+++ 1227.py
@@ -1,3 +1,4 @@
+
 # a = int(input("enter a number: "))
 # b = int(input("enter a number: "))
 # if a > b:
@@ -9,13 +10,84 @@
 #
 #
 
-language = 'Java'
 
-if language == 'Python':
-    print('Language is Python')
-elif language == 'Java':
-    print('Language is java')
-elif language == 'Javascript':
-    print('Language is javascript')
-else:
-    print('No match')
+# language = 'Java'
+#
+# if language == 'Python':
+#     print('Language is Python')
+# elif language == 'Java':
+#     print('Language is java')
+# elif language == 'Javascript':
+#     print('Language is javascript')
+# else:
+#     print('No match')
+
+
+# matchuser = 'admin'
+# loggedin = True
+#
+# if user == 'admin' and loggedin == True:
+#     print('admin page')
+# else:
+#     print('bad credits')
+#
+
+
+# user = 'admin'
+# adminloggedin = False
+#
+# if user == 'admin' or loggedin == True:
+#     print('admin page')
+# else:
+#     print('bad credits')
+
+
+# user = 'admin'
+# loggedin = True
+#
+# if not user == 'admin' or  loggedin == True:
+#     print('admin page')
+# else:
+#     print('bad credits')
+
+# a = [1,2,3]
+# b = [1,2,3]
+# b = a
+#
+# print(id(a))
+# print(id(b))
+# print(a is b)
+
+# condition = True
+#
+# if condition:
+#     print("evaluated to True")
+# else:
+#     print("evaluated to False")
+
+for i in range(9):
+    if i>3:
+        break
+
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1228.py
@@ -0,0 +1,15 @@
+
+##num1 = int(input("enter a number: "))
+##num2 = int(input("enter a number: "))
+##num3 = num1*num2
+##print("product of num1 and num2 is: ", num3)
+
+# num1 = 34
+# if (num1 > 12):
+#     print("num1 is good")
+# elif(num1 > 35):
+#     print("num2 is not gooooo..")
+# else:
+#     print("num2 is great")
+
+
